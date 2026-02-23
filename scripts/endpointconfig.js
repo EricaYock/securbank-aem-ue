f170c936c3712ebff93849bc50c53ac716be8a12
Erica Yock <146484143+EricaYock@users.noreply.github.com>
2026-02-23 14:55:01 -0800

    Update endpointconfig.js

--- scripts/endpointconfig.js
+++ scripts/endpointconfig.js
@@ -1,9 +1,9 @@
 function getAEMPublish() {
-  return 'https://publish-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p178211-e1869897.adobeaemcloud.com';
 }
 
 function getAEMAuthor() {
-  return 'https://author-p137825-e1511252.adobeaemcloud.com';
+  return 'https://author-p178211-e1869897.adobeaemcloud.com';
 }
 
 export { getAEMPublish, getAEMAuthor };
